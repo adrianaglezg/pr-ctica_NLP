49eb99c65b44ae734b2246087cec93306238ecbb
adrirana <158598716+adrianaglezg@users.noreply.github.com>
2024-12-15 22:05:29 +0100

    Add files via upload

--- core.py
+++ core.py
@@ -6,6 +6,13 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import emoji
 
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split
+
+
+# MÓDULO 1
 
 def preprocess_post(text: str) -> str:
     '''
@@ -62,3 +69,126 @@ def preprocess_post(text: str) -> str:
     text = ' '.join(words)
     return text
 
+
+# df['clean_post'] = df['post'].apply(preprocess_post)
+# df.to_csv("processed_dataset.csv", index=False, sep=',', quotechar='"')
+
+# MÓDULO 2
+
+class SubredditClassifier:
+    def __init__(self, max_features=5000, ngram_range=(1, 2), max_iter=500, random_state=42):
+        self.tfidf = TfidfVectorizer(max_features=max_features, ngram_range=ngram_range)
+        self.model = LogisticRegression(max_iter=max_iter, random_state=random_state)
+
+    def train(self, X_train, y_train):
+        """
+        Entrena el modelo utilizando TF-IDF y Logistic Regression.
+        """
+        X_train_tfidf = self.tfidf.fit_transform(X_train)
+        self.model.fit(X_train_tfidf, y_train)
+
+    def classify_subreddit(self, text):
+        """
+        Clasifica un texto en una de las categorías.
+        """
+        text_tfidf = self.tfidf.transform([text])
+        return self.model.predict(text_tfidf)[0]
+
+    def transform_data(self, X_data):
+        """
+        Vectoriza datos de entrada utilizando TF-IDF.
+        """
+        return self.tfidf.transform(X_data)
+    
+
+# MÓDULO 3
+
+def find_subreddit_mentions(text: str) -> list:
+    '''
+    Extract all subreddit mentions in a given text.
+    It searches for patterns like '/r/subreddit'.
+
+    Input:
+    - text (str): The input text to search for subreddit mentions.
+
+    Output:
+    - list: A list containing the mentioned subreddits.
+    '''
+    
+    pattern = r'/r/\w+'
+    
+    matches = re.findall(pattern, text)
+    
+    return matches
+
+def url_extraction(text: str) -> list:
+    '''
+    Extract all URLs in a given text.
+    It searches for patterns that match common URL formats.
+
+    Input:
+    - text (str): The input text to search for URLs.
+
+    Output:
+    - list: A list containing all the extracted URLs.
+    '''
+    pattern = r'(https?://\S+|www\.\S+)'
+    urls = re.findall(pattern, text)
+    return urls
+
+def phone_number_extraction(text: str) -> list:
+    '''
+    Extract phone numbers from a given text.
+    It searches for patterns matching common phone number formats.
+
+    Input:
+    - text (str): The input text to search for phone numbers.
+
+    Output:
+    - list: A list containing all the extracted phone numbers.
+    '''
+    pattern = r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}'
+    phone_numbers = re.findall(pattern, text)
+    return phone_numbers
+
+def dates_extraction(text: str) -> list:
+    '''
+    Extract all dates from a given text.
+    It searches for patterns matching common date formats.
+
+    Input:
+    - text (str): The input text to search for dates.
+
+    Output:
+    - list: A list containing all the extracted dates.
+    '''
+    pattern = r'\b(?:\d{1,2}[-/]\d{1,2}[-/]\d{2,4}|\d{1,2}\s\w+\s\d{4}|\w+\s\d{1,2},\s\d{4})\b'
+    dates = re.findall(pattern, text)
+    return dates
+
+def code_extraction(text: str) -> list:
+    '''
+    Extract code snippets from a given text
+    It searches for:
+    - Blocks of code delimited by triple backticks
+    - Inline code delimited by single backticks
+    - HTML code tags
+    - Common programming patterns
+
+    Input:
+    - text (str): The input text to search for code snippets.
+
+    Outut:
+    - list: A list containing all the extracted code snippets.
+    '''
+    pattern = (
+        r'```[\s\S]*?```|'  
+        r'`[^`]+?`|' 
+        r'<[^>]+>.*?</[^>]+>|'     
+        r'\b(def\s+\w+\(.*?\):|'   # def()
+        r'print\(.+?\)|'           # print()
+        r'\w+\s*=\s*\[.*?\])\b'    #list []
+    )
+
+    code_snippets = [match.group() for match in re.finditer(pattern, text)]
+    return code_snippets
